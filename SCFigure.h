ba0ae6d5e157995c45cc759c86f741cce66879bb
scpark98 <scpark98@gmail.com>
2025-10-19 23:32:17 +0900

    .

--- SCFigure.h
+++ SCFigure.h
@@ -1,3 +1,12 @@
+/*
+-라인부터 다각형까지 템플릿을 제공하고 벡터형태로 그려준다(ppt 도형과 같이)
+
+- CSCFigure를 상속받아 CSCTriangle, CSCRect...등을 만드느냐
+CSCFigure 하나로 하느냐...
+만약 get_area()라는 함수를 구현한다면 if 또는 switch로 구분.
+그럼 제어점은 ?
+*/
+
 #pragma once
 
 #include <afxwin.h>
